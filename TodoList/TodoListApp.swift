043bf0788bce7a7c1dc4a535b38da6053dfa025a
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2023-10-22 11:58:26 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Hadid Hardiansyah Saputra on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
